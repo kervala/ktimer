ce024a98a62e3dd27ed5500ad28c162f2ccfe426
Kervala <983521+kervala@users.noreply.github.com>
2021-05-23 16:17:15 +0200

    Fixed: Translations

---
+++ translations/simpletimer_fr.ts
@@ -0,0 +1,266 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="fr_FR" sourcelanguage="en_US">
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="../ui/mainwindow.ui" line="175"/>
+        <source>&amp;Help</source>
+        <translation>&amp;Aide</translation>
+    </message>
+    <message>
+        <location filename="../ui/mainwindow.ui" line="121"/>
+        <source>Add</source>
+        <translation>Ajouter</translation>
+    </message>
+    <message>
+        <location filename="../ui/mainwindow.ui" line="128"/>
+        <source>Remove</source>
+        <translation>Supprimer</translation>
+    </message>
+    <message>
+        <location filename="../ui/mainwindow.ui" line="135"/>
+        <source>Start</source>
+        <translation>Commencer</translation>
+    </message>
+    <message>
+        <location filename="../ui/mainwindow.ui" line="104"/>
+        <source>Name</source>
+        <translation>Nom</translation>
+    </message>
+    <message>
+        <location filename="../ui/mainwindow.ui" line="18"/>
+        <source>Timers</source>
+        <translation>Chronomètres</translation>
+    </message>
+    <message>
+        <location filename="../ui/mainwindow.ui" line="40"/>
+        <source>Timer</source>
+        <translation>Chronomètre</translation>
+    </message>
+    <message>
+        <location filename="../ui/mainwindow.ui" line="46"/>
+        <source>Delay</source>
+        <translation>Délai</translation>
+    </message>
+    <message>
+        <location filename="../ui/mainwindow.ui" line="68"/>
+        <location filename="../ui/mainwindow.ui" line="85"/>
+        <source>:</source>
+        <translation>:</translation>
+    </message>
+    <message>
+        <location filename="../ui/mainwindow.ui" line="114"/>
+        <source>Color</source>
+        <translation>Couleur</translation>
+    </message>
+    <message>
+        <location filename="../ui/mainwindow.ui" line="162"/>
+        <source>&amp;File</source>
+        <translation>&amp;Fichier</translation>
+    </message>
+    <message>
+        <location filename="../ui/mainwindow.ui" line="196"/>
+        <source>&amp;About...</source>
+        <translation>&amp;A propos...</translation>
+    </message>
+    <message>
+        <location filename="../ui/mainwindow.ui" line="204"/>
+        <source>About Qt</source>
+        <translation>A propos de Qt</translation>
+    </message>
+    <message>
+        <location filename="../ui/mainwindow.ui" line="212"/>
+        <source>Check updates...</source>
+        <translation>Vérifier mises-à-jour...</translation>
+    </message>
+    <message>
+        <location filename="../ui/mainwindow.ui" line="217"/>
+        <source>&amp;Open...</source>
+        <translation>&amp;Ouvrir...</translation>
+    </message>
+    <message>
+        <location filename="../ui/mainwindow.ui" line="222"/>
+        <source>&amp;Save</source>
+        <translation>&amp;Enregistrer</translation>
+    </message>
+    <message>
+        <location filename="../ui/mainwindow.ui" line="227"/>
+        <source>Save &amp;as...</source>
+        <translation>Enregistrer &amp;sous...</translation>
+    </message>
+    <message>
+        <location filename="../ui/mainwindow.ui" line="232"/>
+        <source>&amp;New</source>
+        <translation>&amp;Nouveau</translation>
+    </message>
+    <message>
+        <location filename="../ui/mainwindow.ui" line="188"/>
+        <source>E&amp;xit</source>
+        <translation>&amp;Quitter</translation>
+    </message>
+    <message>
+        <location filename="../src/mainwindow.cpp" line="368"/>
+        <source>About %1</source>
+        <translation>A propos de %1</translation>
+    </message>
+    <message>
+        <location filename="../src/mainwindow.cpp" line="372"/>
+        <source>Author: %1</source>
+        <translation>Auteur : %1</translation>
+    </message>
+    <message>
+        <location filename="../src/mainwindow.cpp" line="373"/>
+        <source>Support: %1</source>
+        <translation>Support technique : %1</translation>
+    </message>
+    <message>
+        <location filename="../ui/mainwindow.ui" line="142"/>
+        <source>Stop</source>
+        <translation>Arrêter</translation>
+    </message>
+    <message>
+        <location filename="../src/mainwindow.cpp" line="162"/>
+        <source>Open timers</source>
+        <translation>Ouvrir les chronomètres</translation>
+    </message>
+    <message>
+        <location filename="../src/mainwindow.cpp" line="162"/>
+        <location filename="../src/mainwindow.cpp" line="181"/>
+        <source>SimpleTimer Files (*.stf)</source>
+        <translation>Fichiers SimpleTimer (*.stf)</translation>
+    </message>
+    <message>
+        <location filename="../src/mainwindow.cpp" line="181"/>
+        <source>Save timers</source>
+        <translation>Enregistrer les chronomètres</translation>
+    </message>
+    <message>
+        <location filename="../src/mainwindow.cpp" line="194"/>
+        <source>LCD color</source>
+        <translation>Couleur LCD</translation>
+    </message>
+    <message>
+        <location filename="../src/mainwindow.cpp" line="301"/>
+        <source>SimpleTimer notification</source>
+        <translation>Notification de SimpleTimer</translation>
+    </message>
+    <message>
+        <location filename="../src/mainwindow.cpp" line="301"/>
+        <source>End of timer %1 after %2. You can restart it if you need.</source>
+        <translation>Fin du chronmètre %1 après %2. Vous pouvez le relancer si besoin.</translation>
+    </message>
+    <message>
+        <location filename="../src/mainwindow.cpp" line="370"/>
+        <source>Simple timers notifier</source>
+        <translation>Notificateur de chronomètres simples</translation>
+    </message>
+    <message>
+        <location filename="../src/mainwindow.cpp" line="433"/>
+        <source>New version</source>
+        <translation>Mise-à-jour disponible</translation>
+    </message>
+    <message>
+        <location filename="../src/mainwindow.cpp" line="434"/>
+        <source>Version %1 is available since %2.
+
+Do you want to download it now?</source>
+        <translation>La version %1 est disponible depuis le %2.
+
+Souhaitez-vous la télécharger maintenant ?</translation>
+    </message>
+    <message>
+        <location filename="../src/mainwindow.cpp" line="455"/>
+        <source>No update found</source>
+        <translation>Aucune mise-à-jour trouvée</translation>
+    </message>
+    <message>
+        <location filename="../src/mainwindow.cpp" line="456"/>
+        <source>You already have the last %1 version (%2).</source>
+        <translation>Vous avez déjà la dernière version de %1 (%2).</translation>
+    </message>
+</context>
+<context>
+    <name>QObject</name>
+    <message>
+        <location filename="../src/utils_x.cpp" line="106"/>
+        <location filename="../src/utils_win.cpp" line="474"/>
+        <source>Whole screen</source>
+        <translation>Ecran complet</translation>
+    </message>
+</context>
+<context>
+    <name>SystrayIcon</name>
+    <message>
+        <location filename="../src/systrayicon.cpp" line="66"/>
+        <source>Minimize</source>
+        <translation>Minimiser</translation>
+    </message>
+    <message>
+        <location filename="../src/systrayicon.cpp" line="67"/>
+        <source>Restore</source>
+        <translation>Restaurer</translation>
+    </message>
+    <message>
+        <location filename="../src/systrayicon.cpp" line="68"/>
+        <source>Quit</source>
+        <translation>Quitter</translation>
+    </message>
+</context>
+<context>
+    <name>TimerModel</name>
+    <message>
+        <location filename="../src/timermodel.cpp" line="95"/>
+        <source>Timer #%1</source>
+        <translation>Chronomètre #%1</translation>
+    </message>
+</context>
+<context>
+    <name>UpdateDialog</name>
+    <message>
+        <location filename="../ui/updatedialog.ui" line="14"/>
+        <source>Download</source>
+        <translation>Télécharger</translation>
+    </message>
+    <message>
+        <location filename="../ui/updatedialog.ui" line="20"/>
+        <source>No text</source>
+        <translation>Pas de texte</translation>
+    </message>
+    <message>
+        <location filename="../ui/updatedialog.ui" line="52"/>
+        <source>Install</source>
+        <translation>Installer</translation>
+    </message>
+    <message>
+        <location filename="../ui/updatedialog.ui" line="59"/>
+        <source>Retry</source>
+        <translation>Réessayer</translation>
+    </message>
+    <message>
+        <location filename="../ui/updatedialog.ui" line="66"/>
+        <source>Cancel</source>
+        <translation>Annuler</translation>
+    </message>
+    <message>
+        <location filename="../src/updatedialog.cpp" line="72"/>
+        <source>Downloading %1...</source>
+        <translation>Téléchargement de %1 en cours...</translation>
+    </message>
+    <message>
+        <location filename="../src/updatedialog.cpp" line="118"/>
+        <source>An error occurred when downloading, please click on &quot;Retry&quot; to restart download.</source>
+        <translation>Une erreur est survenue pendant le téléchargement, veuillez cliquer sur &quot;Réessayer&quot; pour relancer le téléchargement.</translation>
+    </message>
+    <message>
+        <location filename="../src/updatedialog.cpp" line="144"/>
+        <source>Your download is complete, click on &quot;Install&quot; to install the new version.</source>
+        <translation>Votre téléchargement est terminé, cliquez sur &quot;Installer&quot; pour installer la mise-à-jour.</translation>
+    </message>
+    <message>
+        <location filename="../src/updatedialog.cpp" line="149"/>
+        <source>Your download is complete, but we&apos;re unable to create file %1.</source>
+        <translation>Votre téléchargement est terminé, mais impossible de créer le fichier %1.</translation>
+    </message>
+</context>
+</TS>
