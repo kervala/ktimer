25ee9293005dc735af93f43adf86f7417f730248
kervala <kervala@localhost>
2021-05-23 17:16:56 +0200

    Fixed: Duration in popup

--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -295,7 +295,7 @@ void MainWindow::onTimerFinished(int row)
 {
 	Timer& timer = m_model->getTimer(m_selectedTimer);
 
-	SystrayIcon::getInstance()->displayMessage(tr("SimpleTimer notification"), tr("End of timer %1 after %2. You can restart it if you need.").arg(timer.name).arg(timer.getRestString()), SystrayIcon::ActionNone);
+	SystrayIcon::getInstance()->displayMessage(tr("SimpleTimer notification"), tr("End of timer %1 after %2. You can restart it if you need.").arg(timer.name).arg(timer.getDelayString()), SystrayIcon::ActionNone);
 }
 
 void MainWindow::onTimerSelected(const QItemSelection& selected, const QItemSelection& deselected)
