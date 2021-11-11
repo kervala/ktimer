0f4a73edf4129643043aa1b6ba39894e9c01deea
Kervala <983521+kervala@users.noreply.github.com>
2021-11-11 11:55:25 +0100

    Changed: New TimersView to implement QListView

---
+++ src/timersview.cpp
@@ -0,0 +1,47 @@
+/*
+ *  kTimer is a timers manager
+ *  Copyright (C) 2021  Cedric OCHS
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+#include "common.h"
+#include "timersview.h"
+
+TimersView::TimersView(QWidget *parent = nullptr):QListView(parent)
+{
+}
+
+QSize TimersView::sizeHint() const
+{
+	return QSize(width(), minimumSizeHint().height());
+}
+
+QSize TimersView::minimumSizeHint() const
+{
+	if (model()->rowCount() == 0) return QSize(0, 0);
+
+	int count = 0;
+
+	for (int row = 0, nrow = model()->rowCount(); row < nrow; ++row)
+	{
+		if (!isRowHidden(row))
+		{
+			++count;
+		}
+	}
+
+	return QSize(0, count * sizeHintForRow(0));
+}
---
+++ src/timersview.h
@@ -0,0 +1,32 @@
+/*
+ *  kTimer is a timers manager
+ *  Copyright (C) 2021  Cedric OCHS
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+#ifndef TIMERSVIEW_H
+#define TIMERSVIEW_H
+
+class TimersView : public QListView
+{
+public:
+	TimersView(QWidget* parent);
+
+	QSize sizeHint() const override;
+	QSize minimumSizeHint() const override;
+};
+
+#endif
