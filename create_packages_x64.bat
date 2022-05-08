4c1bc51690d125fa11673d07ac8fe71470726491
Kervala <983521+kervala@users.noreply.github.com>
2022-05-08 11:29:56 +0200

    Changed: New version 1.6

--- create_packages_x64.bat
+++ create_packages_x64.bat
@@ -1,13 +1,13 @@
 @echo off
 
 :: To be sure Python and Qt are in PATH
-::set PATH=%PATH%;D:\Tools\Python27;D:\Tools\Python27\Scripts;C:\Prog\Qt\6.0.0\msvc2019_64\bin
-set PATH=%PATH%;D:\Tools\Python27;D:\Tools\Python27\Scripts;C:\Prog\Qt\5.15.2\msvc2019_64\bin
+set PATH=%PATH%;D:\Tools\Python27;D:\Tools\Python27\Scripts;C:\Prog\Qt\6.3.0\msvc2019_64\bin
+::set PATH=%PATH%;D:\Tools\Python27;D:\Tools\Python27\Scripts;C:\Prog\Qt\5.15.2\msvc2019_64\bin
 
 set CURRENTDIR=%cd%
 
 :: Call VC++ command-line tools
-call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvarsall.bat" x64
+call "C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvarsall.bat" x64
 
 cd %CURRENTDIR%
 
