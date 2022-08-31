2c9b69013654e325b115e5124be4d9895f2e74c6
Kervala <983521+kervala@users.noreply.github.com>
2022-08-31 11:06:43 +0200

    Changed: Install runtimes too

--- create_packages_x64.bat
+++ create_packages_x64.bat
@@ -1,7 +1,7 @@
 @echo off
 
 :: To be sure Python and Qt are in PATH
-set PATH=%PATH%;D:\Tools\Python27;D:\Tools\Python27\Scripts;C:\Prog\Qt\6.3.0\msvc2019_64\bin
+set PATH=%PATH%;C:\Prog\Qt\6.3.1\msvc2019_64\bin
 ::set PATH=%PATH%;D:\Tools\Python27;D:\Tools\Python27\Scripts;C:\Prog\Qt\5.15.2\msvc2019_64\bin
 
 set CURRENTDIR=%cd%
@@ -16,7 +16,7 @@ rmdir /s /q package_x64
 mkdir package_x64
 
 cd package_x64
-cmake .. -DCMAKE_BUILD_TYPE=Release -G"NMake Makefiles"
+cmake .. -DCMAKE_BUILD_TYPE=Release -DWITH_INSTALL_RUNTIMES=ON -G"NMake Makefiles"
 
 nmake package
 
